e95178eac11a620a851ca90bdadec4a3f8fa5915
Nang Phyo Thet Thet Khaing <6731503071@lamduan.mfu.ac.th>
2025-08-25 10:00:47 +0000

    push

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,16 +4,54 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
+    private Long id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
     public Concert() {
     }
     public Concert(String title, String description) {
-        //TODO: set attributes
-
+        this.title = title;
+        this.description = description;
     }
- 
     //TODO: add getters and setters for all attributes
     // You can use Source action/generate getter setter function
+   public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+     public String getPerformer() {
+        return performer;
+    }
 
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+      
 }
